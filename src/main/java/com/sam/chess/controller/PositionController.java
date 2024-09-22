bf94d32cf9544e0d6af5f6ec474b884d1e6adc8b
SamuelBanfield <samueljbanfield@gmail.com>
2024-09-22 15:11:03 +0100

    Filter fetched games by source

--- src/main/java/com/sam/chess/controller/PositionController.java
+++ src/main/java/com/sam/chess/controller/PositionController.java
@@ -1,11 +1,16 @@
 package com.sam.chess.controller;
 
+import static com.sam.chess.db.entity.GameMoveRepository.matchesPosition;
+import static com.sam.chess.db.entity.GameMoveRepository.matchesSource;
 import static com.sam.chess.model.Colour.BLACK;
 import static com.sam.chess.model.Colour.WHITE;
+import static java.util.Collections.emptyList;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 import java.io.IOException;
+import java.util.stream.Collectors;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -18,10 +23,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.sam.chess.client.chessdotcom.ChessDotComClient;
 import com.sam.chess.client.lichess.LichessClient;
+import com.sam.chess.db.entity.GameMoveEntity;
+import com.sam.chess.db.entity.GameMoveRepository;
+import com.sam.chess.db.entity.MoveEntity;
 import com.sam.chess.db.entity.MoveRepository;
 import com.sam.chess.model.Colour;
+import com.sam.chess.model.GameResult;
 import com.sam.chess.model.ModelGame;
 import com.sam.chess.model.ModelMove;
+import com.sam.chess.model.Source;
 
 @RestController
 @RequestMapping("/game")
@@ -29,8 +39,8 @@ public class PositionController {
 
   // Regex matching the important features of a FEN
   private static final Pattern GAME_PATTERN = Pattern.compile("([rnbqkpRNBQKP1-8]+/){7}[rnbqkpRNBQKP1-8]+ [wb]");
-  // Regex matching user@lichess or user@chessdotcom
-  private static final Pattern SOURCE_PATTERN = Pattern.compile("^(?<user>[^@]+)@(?<source>lichess|chessdotcom)$");
+
+  record MoveWithFrequency(ModelMove move, int whiteWins, int draws, int blackWins) {};
 
 	@Autowired
 	private LichessClient _lichessClient;
@@ -38,21 +48,21 @@ public class PositionController {
 	private ChessDotComClient _chessDotComClient;
 
   @Autowired
-  private MoveRepository _moveRepository;
+  private GameMoveRepository _gameMoveRepository;
 
-	@RequestMapping(path = "/games/lichess/{userName}", method = GET)
+	@RequestMapping(path = "/lichess/{userName}", method = GET)
 	@ResponseBody
 	public List<ModelGame> getLichessGames(@PathVariable("userName") final String userName) {
 		return _lichessClient.getGames(userName);
 	}
 
-	@RequestMapping(path = "/games/chessdotcom/{userName}", method = GET)
+	@RequestMapping(path = "/chessdotcom/{userName}", method = GET)
 	@ResponseBody
 	public List<ModelGame> getChessDotComGames(@PathVariable("userName") final String userName) throws IOException, InterruptedException {
     return _chessDotComClient.getGames(userName);
 	}
 
-  @RequestMapping(path = "/games", method = GET)
+  @RequestMapping(path = "/", method = GET)
   @ResponseBody
   public List<MoveWithFrequency> gamesFromPosition(
     @RequestParam("fen") final String position,
@@ -65,22 +75,32 @@ public class PositionController {
     }
     final Colour colour = colourString.equals("black") ? BLACK : WHITE;
     final List<Source> sources = sourcesRaw.stream()
-      .map(source -> {
-        final Matcher sourceMatcher = SOURCE_PATTERN.matcher(source);
-        if (!sourceMatcher.find()) {
-          throw new IllegalArgumentException("Invalid source: " + source);
-        }
-        return new Source(sourceMatcher.group("user"), sourceMatcher.group("source"));
+      .map(Source::fromString)
+      .toList();
+
+    Map<String, Map<GameResult, List<GameMoveEntity>>> movesWithFrequencies =  _gameMoveRepository.findAll(matchesPosition(matcher.group()).and(matchesSource(sources, colour))).stream()
+      .collect(Collectors.groupingBy(
+        move -> move.getMove().name(),
+        Collectors.groupingBy(
+          move -> move.getGame().getResult()
+        )
+      ));
+
+    return movesWithFrequencies.values().stream()
+      .map(frequencies -> {
+        MoveEntity move = frequencies.values().stream()
+          .flatMap(List::stream)
+          .findFirst()
+          .orElseThrow(() -> new IllegalStateException("No moves for entry"))
+          .getMove();
+        
+        return new MoveWithFrequency(
+          new ModelMove(move.name(), move.start(), move.end()),
+          frequencies.getOrDefault(GameResult.WHITE_WIN, emptyList()).size(),
+          frequencies.getOrDefault(GameResult.DRAW, emptyList()).size(),
+          frequencies.getOrDefault(GameResult.BLACK_WIN, emptyList()).size());
       })
       .toList();
-    return _moveRepository.findAllByStartLike(matcher.group() + "%")
-      .stream()
-      .map(move -> new MoveWithFrequency(new ModelMove(move.name(), move.start(), move.end()), move.whiteWins(), move.draws(), move.blackWins()))
-      .toList();    
   }
-
-  record Source(String user, String site) {};
-
-  record MoveWithFrequency(ModelMove move, int whiteWins, int draws, int blackWins) {};
 
 }
