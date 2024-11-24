93866a620a7a021cbb4521c2be6edbc1f2aae141
SamuelBanfield <samueljbanfield@gmail.com>
2024-11-24 21:08:44 +0000

    Add test for lichess client with test user

--- src/test/java/com/sam/chess/client/lichess/TestLichessClient.java
+++ src/test/java/com/sam/chess/client/lichess/TestLichessClient.java
@@ -1,11 +1,19 @@
 package com.sam.chess.client.lichess;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.sam.chess.model.GameResult;
+import com.sam.chess.model.ModelGame;
+
 /**
  * Tests for {@link LichessClient}.
+ * The "samjban_test" account has 2 wins and one loss.
  */
 @SpringBootTest
 public class TestLichessClient {
@@ -15,7 +23,10 @@ public class TestLichessClient {
 
     @Test
     void testLichessClient() {
-      System.out.println(_lichessClient.getGames("samjban"));
+      final List<ModelGame> games = _lichessClient.getGames("samjban_test");
+      assertEquals(3, games.size());
+      assertEquals(2, games.stream().filter(game -> game.result().equals(GameResult.WHITE_WIN)).count());
+      assertEquals(1, games.stream().filter(game -> game.result().equals(GameResult.BLACK_WIN)).count());
     }
 
 }
